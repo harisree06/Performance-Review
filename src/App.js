d2fd3cc5243e7575b3a34bf9af5ae954d1309b72
harisree06 <harisree.n.s@gmail.com>
2018-08-01 21:56:22 -0400

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import Nav from './components/navBar/Navbar'
 import C from './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
+      <Nav/>
       <div className={C.App}>
         <h1>Performance Review</h1>
       </div>
+      </div>
+
     );
   }
 }
-
-//hihihihihskqnksnkjnsk
 
 export default App;
