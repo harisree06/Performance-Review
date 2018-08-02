db82dd37f7cebca4aab7cb00963c67d92daad946
harisree06 <harisree.n.s@gmail.com>
2018-08-01 20:41:30 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import C from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={C.App}>
+        <h1>Performance Review</h1>
+      </div>
+    );
+  }
+}
+
+//hihihihihskqnksnkjnsk
+
+export default App;
